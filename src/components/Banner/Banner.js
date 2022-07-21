233c1a611a3da6f27ec73df57d83afad9b99fcbc
Himanshu394 <56669185+Himanshu394@users.noreply.github.com>
2022-07-21 17:34:52 +0530

    Add files via upload

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,65 @@
+import { Container, makeStyles, Typography } from "@material-ui/core";
+import Carousel from "./Carousel";
+
+const useStyles = makeStyles((theme) => ({
+  banner: {
+    backgroundImage:
+      "url('https://images.unsplash.com/photo-1639322537228-f710d846310a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmxvY2tjaGFpbnxlbnwwfHwwfHw%3D&w=1000&q=80')"
+  },
+  bannerContent: {
+    height: 400,
+    display: "flex",
+    flexDirection: "column",
+    paddingTop: 25,
+    justifyContent: "space-around"
+  },
+  tagline: {
+    display: "flex",
+    height: "40%",
+    flexDirection: "column",
+    justifyContent: "center",
+    textAlign: "center"
+  },
+  carousel: {
+    height: "50%",
+    display: "flex",
+    alignItems: "center"
+  }
+}));
+
+function Banner() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.banner}>
+      <Container className={classes.bannerContent}>
+        <div className={classes.tagline}>
+          <Typography
+            variant="h2"
+            style={{
+              fontWeight: "bold",
+              marginBottom: 15,
+              fontFamily: "Montserrat",
+              color: "yellowgreen"
+            }}
+          >
+            Crypto Tracker
+          </Typography>
+          <Typography
+            variant="subtitle2"
+            style={{
+              color: "darkgrey",
+              textTransform: "capitalize",
+              fontFamily: "Montserrat"
+            }}
+          >
+            Get all the Info regarding your favorite Crypto Currency
+          </Typography>
+        </div>
+        <Carousel />
+      </Container>
+    </div>
+  );
+}
+
+export default Banner;
